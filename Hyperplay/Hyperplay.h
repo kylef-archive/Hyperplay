aba1d4b6fc22fdbb6a84a18d3ee0890cb9939afa
Kyle Fuller <kyle@fuller.li>
2015-11-17 12:44:32 +0300

    Initial Xcode project

---
+++ Hyperplay/Hyperplay.h
@@ -0,0 +1,19 @@
+//
+//  Hyperplay.h
+//  Hyperplay
+//
+//  Created by Kyle Fuller on 17/11/2015.
+//  Copyright © 2015 Apiary. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Hyperplay.
+FOUNDATION_EXPORT double HyperplayVersionNumber;
+
+//! Project version string for Hyperplay.
+FOUNDATION_EXPORT const unsigned char HyperplayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hyperplay/PublicHeader.h>
+
+
